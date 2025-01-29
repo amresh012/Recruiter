9f01d077193a6bff2c9ed8a61f2e83e08c9e2091
amresh012 <73485115+amresh012@users.noreply.github.com>
2025-01-29 14:02:41 +0530

    initialize server with basic structure and user model

---
+++ Server/model/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    compnay: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    designation: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    Verified: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
